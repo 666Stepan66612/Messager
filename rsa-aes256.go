b664261a55008359c46b0431af12a1cef6257f09
Stepan <lskuku1211@gmail.com>
2025-12-09 23:00:43 +0300

    comment cycle

--- rsa-aes256.go
+++ rsa-aes256.go
@@ -11,6 +11,17 @@ import (
 	"time"
 )
 
+//This is rsa-aes cycle: When a user writes a message, they send a request to another user to generate two RSA keys.
+//  After generation, the sender of the message receives the public key from the intended recipient,
+//  which does not need to be hidden from a third-party observer.
+//  Next, the sender applies the AES256 algorithm to their message,
+//  resulting in encrypted content, which also does not need to be hidden, and sends it to the recipient.
+//  Then, using the public RSA key received from the recipient, the sender encrypts the temporary key used for encrypting their message.
+//  The resulting ciphertext from applying the RSA public key to the temporary key also does not need to be hidden and is sent to the recipient.
+//  The recipient receives the encrypted message (which was encrypted using the public AES256 key)
+//  and the AES256 key in encrypted form (encrypted with the public RSA key).
+//  They decrypt the AES256 key using their private RSA key and apply it to the encrypted message.
+
 type rsaEnv struct {
 	n *big.Int
 	e *big.Int
@@ -27,7 +38,7 @@ type privateRSAkey struct {
 	n *big.Int
 }
 
-func (r *rsaEnv) SetRSAEnv() error {
+func (r *rsaEnv) SetRSAEnv() error { // create new rsa env
 	key, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		panic(err)
@@ -40,25 +51,25 @@ func (r *rsaEnv) SetRSAEnv() error {
 	return nil
 }
 
-func (p *publicRSAkey) SetPublicRSAkeyEnv(rsa rsaEnv) {
+func (p *publicRSAkey) SetPublicRSAkeyEnv(rsa rsaEnv) { // set public key env from rsa struct
 	p.e = rsa.e
 	p.n = rsa.n
 }
 
-func (p *privateRSAkey) SetPrivateRSAkeyEnv(rsa rsaEnv) {
+func (p *privateRSAkey) SetPrivateRSAkeyEnv(rsa rsaEnv) { // set private key env from rsa struct
 	p.d = rsa.d
 	p.n = rsa.n
 }
 
-func (p *publicRSAkey) GetPublicRSAkeyEnv() (*big.Int, *big.Int) {
+func (p *publicRSAkey) GetPublicRSAkeyEnv() (*big.Int, *big.Int) { // get public key env for rsa
 	return p.e, p.n
 }
 
-func (p *privateRSAkey) GetPrivateRSAkeyEnv() (*big.Int, *big.Int) {
+func (p *privateRSAkey) GetPrivateRSAkeyEnv() (*big.Int, *big.Int) { // get private key env for rsa
 	return p.d, p.n
 }
 
-func AES256Encrypt(key, plaintext []byte) []byte {
+func AES256Encrypt(key, plaintext []byte) []byte { // aes encrypts the message that the user wants to send with the key that aes encryption will be encrypted with the public key rsa
 	block, _ := aes.NewCipher(key)
 	gcm, _ := cipher.NewGCM(block)
 	nonce := make([]byte, gcm.NonceSize())
@@ -66,7 +77,7 @@ func AES256Encrypt(key, plaintext []byte) []byte {
 	return gcm.Seal(nonce, nonce, plaintext, nil)
 }
 
-func AES256Decrypt(key, ciphertext []byte) ([]byte, error) {
+func AES256Decrypt(key, ciphertext []byte) ([]byte, error) { // after the recipient receives the encrypted message, aes decrypts the encrypted message using the same key
 	block, _ := aes.NewCipher(key)
 	gcm, _ := cipher.NewGCM(block)
 	nonceSize := gcm.NonceSize()
@@ -76,7 +87,7 @@ func AES256Decrypt(key, ciphertext []byte) ([]byte, error) {
 
 func main() {
 	start := time.Now()
-	message := "Hardest hello world!"
+	message := "Hardest hello world!" // create message
 	rsa := rsaEnv{}
 	var publicRSAKey publicRSAkey
 	var privateRSAKey privateRSAkey
@@ -87,17 +98,17 @@ func main() {
 	privateRSAKey.SetPrivateRSAkeyEnv(rsa)
 	privateRSAKey.GetPrivateRSAkeyEnv()
 
-	aesKey := make([]byte, 32)
+	aesKey := make([]byte, 32) // create aes key
 	rand.Read(aesKey)
 
-	publicTextUnderAES := AES256Encrypt(aesKey, []byte(message))
+	publicTextUnderAES := AES256Encrypt(aesKey, []byte(message)) // 1. encrypt message by aes key; 2. send to message recipient an encrypted message to the recipient, it is not secret
 
 	aesKeyBig := new(big.Int).SetBytes(aesKey)
-	encryptedAESKey := new(big.Int).Exp(aesKeyBig, publicRSAKey.e, publicRSAKey.n)
+	encryptedAESKey := new(big.Int).Exp(aesKeyBig, publicRSAKey.e, publicRSAKey.n) // 3. apply a public rsa key to the aes key; 4. send the aes key in encrypted form, it is not secret
 
-	decryptedAESKey := new(big.Int).Exp(encryptedAESKey, privateRSAKey.d, privateRSAKey.n)
+	decryptedAESKey := new(big.Int).Exp(encryptedAESKey, privateRSAKey.d, privateRSAKey.n) // 5. decrypt the aes key we obtained in step 4 using the private rsa key
 
-	decrypt, err := AES256Decrypt(decryptedAESKey.Bytes(), publicTextUnderAES)
+	decrypt, err := AES256Decrypt(decryptedAESKey.Bytes(), publicTextUnderAES) // 6. decrypt the encrypted message from step 2 using the decrypted rsa key
 	if err != nil {
 		panic(err)
 	}
