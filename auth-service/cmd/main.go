7097ec1cbb2afa0fe2895e4de45ee8676066fd24
Stepan <lskuku1211@gmail.com>
2025-12-16 23:51:39 +0300

    auth-service add main and dockerfile

---
+++ auth-service/cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"messenger/auth-service/internal/handlers"
+    "messenger/auth-service/internal/repositories"
+    "messenger/auth-service/internal/services"
+)
+
+func main(){
+	dsn := os.Getenv("DB_DSN")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil{
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	rdb := redis.NewClient(&redis.Option{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: "",
+		DB:       0,
+	})
+
+	db.AutoMigrate(&models.User{}, &models.Session{})
+
+	userRepo := repositories.NewUserRepository(db)
+	session := repositories.NewSessionRepository(rdb)
+	authService := service.NewAuthService(userRepo, sessionRepo)
+	authHandler := handlers.NewAuthHandler(authService)
+
+	r := gin.Default()
+
+	r.POST("/register", authHandler.Register)
+	r.POST("/login", authHandler.Login)
+	r.POST("/logout", authHandler.Logout)
+	r.POST("/verify", authHandler.VerifyToken)
+
+	r.Run(":9085")
+}
